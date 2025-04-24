f938eafb6a928830fb9460a9185dfc25d6c21de8
Brandon Hopkins <76761586+TechHutTV@users.noreply.github.com>
2025-04-24 16:41:18 -0700

    Update run_post_process.sh

--- run_post_process.sh
+++ run_post_process.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 exec > "/var/log/jellyfin/$(date +"%Y-%m-%d_%H-%M-%S")-run_post_process-sh.log" 2>&1
 echo $1
-/usr/local/bin/python3 ./record_post_process.py "$1"
+/usr/local/bin/python3 /jellyfin/scripts/record_post_process.py "$1"
+#directories are a work in progress
